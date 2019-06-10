80704f0db518085e9be29339070171c00f011860
Rodrigo <ribocaz@uc.cl>
2019-06-10 12:26:20 -0400

    Added index view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def query(definition, variables = {})
+    response = IIC3103SwapiGraphql::Client.query(definition, variables: variables)
+
+    if response.errors.any?
+      raise QueryError.new(response.errors[:data].join(", "))
+    else
+      response.data
+    end
+  end
 end
--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,5 +1,23 @@
 class MovieController < ApplicationController
+  AllMoviesQuery = IIC3103SwapiGraphql::Client.parse <<-'GRAPHQL'
+    query {
+      allFilms {
+        films {
+          title
+          episodeID
+          openingCrawl
+          director
+          producers
+          releaseDate
+        }
+      }
+    }
+    GRAPHQL
+
   def index
+    data = query AllMoviesQuery
+
+    @movies = data.all_films.films
   end
 
   def show
