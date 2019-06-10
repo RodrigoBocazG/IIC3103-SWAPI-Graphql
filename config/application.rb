669659976adcf4ada30575b52346cba19bf59617
Rodrigo <ribocaz@uc.cl>
2019-06-10 11:57:43 -0400

    Added graphql client config

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
+require "graphql/client"
+require "graphql/client/http"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -16,4 +18,15 @@ module IIC3103SwapiGraphql
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+
+  HTTP = GraphQL::Client::HTTP.new(ENV["API_URL"]) do
+    def headers(context)
+      # Optionally set any HTTP headers
+      { "User-Agent": "My Client" }
+    end
+  end
+
+  Schema = GraphQL::Client.load_schema(HTTP)
+
+  Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
 end
