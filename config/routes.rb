05e8264164e91bef578fd0bf904fea11a8416c7a
Rodrigo <ribocaz@uc.cl>
2019-06-10 11:40:17 -0400

    Copied routes from t1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'starship/show'
-  get 'planet/show'
-  get 'people/show'
-  get 'movie/index'
-  get 'movie/show'
+  get "/planet/:planet_id", to: "planet#show", as: :planet_show
+  get "/people/:people_id", to: "people#show", as: :people_show
+  get "/starship/:starship_id", to: "starship#show", as: :starship_show
+  get "/movie/:episode_id", to: "movie#show", as: :movie_show
+  get "/movies", to: "movie#index"
+  root to: "movie#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
