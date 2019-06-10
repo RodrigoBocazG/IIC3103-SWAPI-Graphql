cae2dfdd5baa02bc0c327903c4a3b28ad7f75b84
Rodrigo <ribocaz@uc.cl>
2019-06-10 17:51:03 -0400

    Added person query

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,38 @@
 class PeopleController < ApplicationController
+  PersonQuery = IIC3103SwapiGraphql::Client.parse <<-'GRAPHQL'
+    query ($id: ID, $personID: ID){
+      person(id: $id, personID: $personID) {
+        id
+        name
+        birthYear
+        eyeColor
+        gender
+        hairColor
+        height
+        mass
+        skinColor
+        homeworld {
+          id
+          name
+        }
+        filmConnection {
+          films {
+            id
+            title
+          }
+        }
+        starshipConnection {
+          starships {
+            id
+            name
+          }
+        }
+      }
+    }
+    GRAPHQL
+
   def show
+    data = query PersonQuery, id: params[:people_id]
+    @person = data.film
   end
 end
