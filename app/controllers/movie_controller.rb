ef80c84c4947ace703536f07c70ff53487aad30f
Rodrigo <ribocaz@uc.cl>
2019-06-10 17:40:23 -0400

    Changed id for films in parameters. Added Movie show page

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -3,6 +3,7 @@ class MovieController < ApplicationController
     query {
       allFilms {
         films {
+          id
           title
           episodeID
           openingCrawl
@@ -16,10 +17,43 @@ class MovieController < ApplicationController
 
   def index
     data = query AllMoviesQuery
-
     @movies = data.all_films.films
   end
 
+  FilmQuery = IIC3103SwapiGraphql::Client.parse <<-'GRAPHQL'
+    query ($id: ID, $filmID: ID){
+      film(id: $id, filmID: $filmID) {
+        id
+        title
+        episodeID
+        openingCrawl
+        director
+        producers
+        releaseDate
+        starshipConnection {
+          starships {
+            id
+            name
+          }
+        }
+        characterConnection {
+          characters {
+            id
+            name
+          }
+        }
+        planetConnection {
+          planets {
+            id
+            name
+          }
+        }
+      }
+    }
+    GRAPHQL
+
   def show
+    data = query FilmQuery, id: params[:film_id]
+    @movie = data.film
   end
 end
