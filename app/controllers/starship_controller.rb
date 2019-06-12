75b592e8735ef7aeb4422a22832ec8167c92b292
Rodrigo <ribocaz@uc.cl>
2019-06-11 21:30:45 -0400

    Added starship view and controller

--- app/controllers/starship_controller.rb
+++ app/controllers/starship_controller.rb
@@ -1,4 +1,39 @@
 class StarshipController < ApplicationController
+  StarShipQuery = IIC3103SwapiGraphql::Client.parse <<-'GRAPHQL'
+    query ($id: ID, $starshipID: ID){
+      starship(id: $id, starshipID: $starshipID) {
+        id
+        name
+        model
+        starshipClass
+        manufacturers
+        costInCredits
+        length
+        crew
+        passengers
+        maxAtmospheringSpeed
+        hyperdriveRating
+        MGLT	
+        cargoCapacity
+        consumables
+        pilotConnection{
+          pilots{
+            id
+            name
+          }
+        }
+        filmConnection {
+          films {
+            id
+            title
+          }
+        }
+      }
+    }
+    GRAPHQL
+
   def show
+    data = query StarShipQuery, id: params[:starship_id]
+    @starship = data.starship
   end
 end
