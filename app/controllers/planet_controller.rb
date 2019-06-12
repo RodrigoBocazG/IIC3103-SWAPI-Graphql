bb772af981f8144b0ea599337bacf31ef6c0d983
Rodrigo <ribocaz@uc.cl>
2019-06-11 21:49:32 -0400

    Added planet controller, query and view

--- app/controllers/planet_controller.rb
+++ app/controllers/planet_controller.rb
@@ -1,4 +1,35 @@
 class PlanetController < ApplicationController
+  PlanetQuery = IIC3103SwapiGraphql::Client.parse <<-'GRAPHQL'
+    query ($id: ID, $planetID: ID){
+      planet(id: $id, planetID: $planetID) {
+        id
+        name
+        diameter
+        rotationPeriod
+        orbitalPeriod
+        gravity
+        population
+        climates
+        terrains
+        surfaceWater
+        residentConnection {
+          residents {
+            id
+            name
+          }
+        }
+        filmConnection{
+          films{
+            id
+            title
+          }
+        }
+      }
+    }
+    GRAPHQL
+
   def show
+    data = query PlanetQuery, id: params[:planet_id]
+    @planet = data.planet
   end
 end
